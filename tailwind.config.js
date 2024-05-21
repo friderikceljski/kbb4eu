40d9a16457f5232e07f327198cf1da4c77befa32
Gasper Rataj <rataj.gasper@gmail.com>
2024-05-22 00:23:31 +0200

    added gradient colors to tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cerise: "#D921A6",
+        pacific: "#31AFD4",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
