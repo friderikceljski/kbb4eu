cf4e91c6ee9c616571335eb8d9de7211fe8ec341
Gasper Rataj <rataj.gasper@gmail.com>
2024-05-22 01:04:27 +0200

    added montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+import { setTransitionHooks } from 'vue';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: '"Montserrat"'
+      },
       colors: {
         cerise: "#D921A6",
         pacific: "#31AFD4",
