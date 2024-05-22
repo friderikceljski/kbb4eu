340aaf724dbb4265ff868fd97dad9fec420bcf36
Gasper Rataj <rataj.gasper@gmail.com>
2024-05-22 22:00:38 +0200

    containerize the texts

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -8,9 +8,11 @@ const bgImageStyle = computed(() => ({
 </script>
 
 <template>
-  <div class="z-0 bg-top bg-cover h-screen grid content-center p-5 gap-2" :style="bgImageStyle">
-    <h1 class="text-5xl font-extrabold text-white">Katja Berk Bevc</h1>
-    <h2 class="text-xl font-light text-white uppercase">kandidatka za poslanko v EP</h2>
-    <HeroButton />
+  <div class="z-0 bg-top bg-cover h-screen grid content-center p-5" :style="bgImageStyle">
+    <div class="container mx-auto space-y-2">
+      <h1 class="text-5xl font-extrabold text-white">Katja Berk Bevc</h1>
+      <h2 class="text-xl font-light text-white uppercase">kandidatka za poslanko v EP</h2>
+      <HeroButton />
+    </div>
   </div>
 </template>
