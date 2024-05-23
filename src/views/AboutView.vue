9dc16637240f8440f8f986c6c051dd7d72e27820
Gasper Rataj <rataj.gasper@gmail.com>
2024-05-23 19:13:41 +0200

    aboutview almost finished for all screens

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,24 +3,28 @@
 
 <template>
   <div class="relative">
-    <img src="@/assets/72.png" alt="Katja Berk Bevc" class="z-0 bottom-0 opacity-25 absolute">
-    <div class="h-screen container p-4 z-10">
-      <h1 class="text-pacific font-extrabold text-6xl">Kdo sem?</h1>
-      <p>
-        Prihajam iz Lesičnega, majhnega kraja sredi pravljičnega Kozjanskega. Domači hribčki z vinogradi in gozdovi so
-        mi
-        zelo pri srcu, že od nekdaj pa rada spoznavam tudi bolj oddaljene kraje. Tudi zato sem se odločila za študij
-        jezikov in magistrirala iz tolmačenja za kombinacijo slovenščina, angleščina in nemščina.
-      </p>
-      <p>Nekaj časa sem delovala kot samostojna tolmačka in prevajalka, politične izkušnje pa sem nabirala v treh
-        različnih parlamentih - v evropskem, nemškem in slovenskem. Trenutno sem zaposlena v državnem zboru kot
-        strokovna
-        sodelavka v poslanski skupini NSi, kjer se ukvarjam s področji zdravstva, izobraževanja, družine in kulture.</p>
-      <p>
-        Od leta 2019 vodim Mlado Slovenijo, podmladek NSi, kjer mlade spodbujamo k aktivnemu delovanju v družbi in jih
-        izobražujemo za delo v skupno dobro. Ponosna sem na naše mlade člane, ki so dejavni občinski svetniki, in vse,
-        ki
-        na različne načine prispevajo k boljšemu življenju v svojih domačih okoljih, v družinah, šolah in službah.</p>
+    <div class="container mx-auto h-screen p-4 z-10 flex sm:items-center sm:py-0 sm:px-5 md:text-right lg:text-left">
+      <img src="@/assets/72.png" alt="Katja Berk Bevc"
+        class="max-h-screen z-0 bottom-0 opacity-25 absolute sm:self-end md:opacity-100 md:static sm:h-3/5 md:h-2/5 lg:h-3/5">
+      <div class="z-0 text-sm leading-snug space-y-3 self-center sm:h-min">
+        <h1 class="text-pacific font-extrabold text-6xl">Kdo sem?</h1>
+        <p>
+          Prihajam iz Lesičnega, majhnega kraja sredi pravljičnega Kozjanskega. Domači hribčki z vinogradi in gozdovi so
+          mi
+          zelo pri srcu, že od nekdaj pa rada spoznavam tudi bolj oddaljene kraje. Tudi zato sem se odločila za študij
+          jezikov in magistrirala iz tolmačenja za kombinacijo slovenščina, angleščina in nemščina.
+        </p>
+        <p>Nekaj časa sem delovala kot samostojna tolmačka in prevajalka, politične izkušnje pa sem nabirala v treh
+          različnih parlamentih - v evropskem, nemškem in slovenskem. Trenutno sem zaposlena v državnem zboru kot
+          strokovna
+          sodelavka v poslanski skupini NSi, kjer se ukvarjam s področji zdravstva, izobraževanja, družine in kulture.
+        </p>
+        <p>
+          Od leta 2019 vodim Mlado Slovenijo, podmladek NSi, kjer mlade spodbujamo k aktivnemu delovanju v družbi in jih
+          izobražujemo za delo v skupno dobro. Ponosna sem na naše mlade člane, ki so dejavni občinski svetniki, in vse,
+          ki
+          na različne načine prispevajo k boljšemu življenju v svojih domačih okoljih, v družinah, šolah in službah.</p>
+      </div>
     </div>
   </div>
 </template>
