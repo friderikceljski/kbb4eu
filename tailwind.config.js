6cb0379574dd84340de39811c2e195ec9e62d80b
Gasper Rataj <rataj.gasper@gmail.com>
2024-06-03 21:00:36 +0200

    added cursive font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: '"Montserrat"'
+        sans: '"Montserrat"',
+        cursive: '"Dynalight"'
       },
       colors: {
         cerise: "#D921A6",
