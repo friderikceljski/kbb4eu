1efcf3bd490ab9c58cd7415a87d58f592c2f6438
Gasper Rataj <rataj.gasper@gmail.com>
2024-06-03 19:25:16 +0200

    write mockup for principles. Needs desktop integration

--- src/views/PrinciplesView.vue
+++ src/views/PrinciplesView.vue
@@ -2,17 +2,29 @@
 </script>
 
 <template>
-    <div class="h-screen grid grid-rows-3">
-        <div class="">
-            <h1 class="text-pacific font-extrabold text-3xl">Podeželje.</h1>
+    <div class="h-screen grid grid-rows-3 sm:grid-cols-3">
+        <div class="p-6 mt-4"> 
+            <div class="space-y-4">
+                <h1 class="text-pacific font-extrabold text-4xl">Podeželje.</h1>
+                <p class="">Ker je v Bruslju premalo razumevanja za podeželje in območja, kot je Kozjansko. Želim
+                    poskrbeti za to, da
+                    EU ob načrtovanju zelenega prehoda gleda tudi na preživetje kmetij in podjetij.</p>
+            </div>
         </div>
-        <div class="">
-            <h1>Konkurenčnost.</h1>
-
+        <div class="bg-neutral-100 p-6 flex items-center">
+            <div class="space-y-4">
+                <h1 class="text-pacific font-extrabold text-4xl">Konkurenčnost.</h1>
+                <p>Ker Evropa po konkurenčnosti vse bolj zaostaja za Ameriko in Kitajsko. Želim delati za to, da lahko
+                    ljudje svoje dobre ideje uresničujejo brez nepotrebnih birokratskih ovir.</p>
+            </div>
         </div>
-        <div class="">
-            <h1>Varnost.</h1>
-
+        <div class="bg-alice p-6 flex items-center">
+            <div class="space-y-4">
+                <h1 class="text-pacific font-extrabold text-4xl">Varnost.</h1>
+                <p>Ker se ljudje ob meji, vse bolj pa tudi v prestolnici zaradi migracij in vojn v bližini ne počutijo več
+                    varne. Varnost je predpogoj za blaginjo, zato želim prispevati k temu, da smo v Sloveniji in Evropi
+                    varni ter imamo nadzor nad migracijami.</p>
+            </div>
         </div>
     </div>
 </template>
