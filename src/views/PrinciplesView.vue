109ffb51a3a81a869f44db41933dce7f610fdcda
Gasper Rataj <rataj.gasper@gmail.com>
2024-06-03 20:11:17 +0200

    rewritten principles and made it desktop friendly

--- src/views/PrinciplesView.vue
+++ src/views/PrinciplesView.vue
@@ -2,29 +2,26 @@
 </script>
 
 <template>
-    <div class="h-screen grid grid-rows-3 sm:grid-cols-3">
-        <div class="p-6 mt-4"> 
-            <div class="space-y-4">
-                <h1 class="text-pacific font-extrabold text-4xl">Podeželje.</h1>
+    <div class="h-screen">
+        <div class="flex h-full flex-col md:flex-row md:h-auto">
+            <div class="flex-1 flex flex-col justify-center md:justify-start p-4 md:px-7 md:py-14">
+                <h2 class="text-pacific font-extrabold text-4xl">Podeželje.</h2>
                 <p class="">Ker je v Bruslju premalo razumevanja za podeželje in območja, kot je Kozjansko. Želim
-                    poskrbeti za to, da
-                    EU ob načrtovanju zelenega prehoda gleda tudi na preživetje kmetij in podjetij.</p>
+                    poskrbeti za to, da EU ob načrtovanju zelenega prehoda gleda tudi na preživetje kmetij in podjetij
+                </p>
             </div>
-        </div>
-        <div class="bg-neutral-100 p-6 flex items-center">
-            <div class="space-y-4">
-                <h1 class="text-pacific font-extrabold text-4xl">Konkurenčnost.</h1>
-                <p>Ker Evropa po konkurenčnosti vse bolj zaostaja za Ameriko in Kitajsko. Želim delati za to, da lahko
-                    ljudje svoje dobre ideje uresničujejo brez nepotrebnih birokratskih ovir.</p>
+            <div class="flex-1 flex flex-col justify-center md:justify-start p-4 md:px-7 md:py-14 bg-neutral-100">
+                <h2 class="text-pacific font-extrabold text-4xl">Konkurenčnost.</h2>
+                <p class="">Ker Evropa po konkurenčnosti vse bolj zaostaja za Ameriko in Kitajsko. Želim delati za to,
+                    da lahko ljudje svoje dobre ideje uresničujejo brez nepotrebnih birokratskih ovir.</p>
             </div>
-        </div>
-        <div class="bg-alice p-6 flex items-center">
-            <div class="space-y-4">
-                <h1 class="text-pacific font-extrabold text-4xl">Varnost.</h1>
-                <p>Ker se ljudje ob meji, vse bolj pa tudi v prestolnici zaradi migracij in vojn v bližini ne počutijo več
-                    varne. Varnost je predpogoj za blaginjo, zato želim prispevati k temu, da smo v Sloveniji in Evropi
-                    varni ter imamo nadzor nad migracijami.</p>
+            <div class="flex-1 flex flex-col justify-center md:justify-start p-4 md:px-7 md:py-14 bg-alice">
+                <h2 class="text-pacific font-extrabold text-4xl">Varnost.</h2>
+                <p class="">Ker se ljudje ob meji, vse bolj pa tudi v prestolnici zaradi migracij in vojn v bližini ne
+                    počutijo več varne. Varnost je predpogoj za blaginjo, zato želim prispevati k temu, da smo v
+                    Sloveniji in Evropi varni ter imamo nadzor nad migracijami.</p>
             </div>
         </div>
     </div>
+
 </template>
